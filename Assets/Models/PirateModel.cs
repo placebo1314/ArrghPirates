f3b03c685f71cd5ffa0f369f84ca6769155b7757
Krisz <placebo1314@gmail.com>
2023-01-10 14:47:47 +0100

    Added Ship/Pirate models

---
+++ Assets/Models/PirateModel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateModel
+{
+    [SerializeField] public string pirateName; // { get; private set; }
+    [SerializeField] public string special;
+    [SerializeField] public string damage;
+    [SerializeField] public string armor;
+    [SerializeField] public string watchDistance;
+    [SerializeField] public string shootDistance;
+    [SerializeField] public string shipNumber;
+}
---
+++ Assets/Models/ShipModel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipModel
+{
+    [SerializeField] public string shipNumber;
+    [SerializeField] public string shipName;
+    [SerializeField] public string damage;
+    [SerializeField] public string armor;
+    [SerializeField] public string watchDistance;
+    [SerializeField] public string shootDistance;
+}

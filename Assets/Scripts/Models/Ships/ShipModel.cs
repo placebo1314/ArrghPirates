908372af0ad596ddc98762ae8c5b7a3568454eb4
NemethBalint96 <nemeth.balint1996@gmail.com>
2023-01-12 18:22:29 +0100

    add pirte and ship types

---
+++ Assets/Scripts/Models/Ships/ShipModel.cs
@@ -0,0 +1,9 @@
+public class ShipModel
+{
+    public string shipId;
+    public string shipName;
+    public string damage;
+    public string armor;
+    public string watchDistance;
+    public string shootDistance;
+}
